b478eed42d5805b0213014eb381407aba8e3e77c
AISNERR <21AISNERR@usergithub.com>
2021-10-12 15:35:49 +0300

    part1

--- libft/part1/ft_strlcpy.c
+++ libft/part1/ft_strlcpy.c
@@ -1,7 +1,5 @@
 #include "libft.h"
 
-#include "libft.h"
-
 size_t  ft_strlcpy(char *dst, const char *src, size_t size)
 {
 	size_t	i;
@@ -14,9 +12,9 @@ size_t  ft_strlcpy(char *dst, const char *src, size_t size)
 			dst[i] = src[i];
 			i++;
 		}
-		dst[i] = 0;
+	    dst[i] = 0;
 	}
 	while (src[i])
-		i++;
+        i++;
 	return (i);
 }
